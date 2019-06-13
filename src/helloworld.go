73d88992f375943fbab663ca6bc45794af8cbc11
zhangtbj <zhangtbj@cn.ibm.com>
2019-06-13 13:42:19 +0800

    first commit

---
+++ src/helloworld.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+)
+
+// Calculate pi using Gregory-Leibniz series:   (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13) - (4/15) ...
+func say(word string) string {
+    title := "Hello "
+    return title + word
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    log.Print("Say something!")
+    word := r.URL.Query()["word"][0]
+    fmt.Fprintf(w, "%s", say(word))
+}
+
+func main() {
+    log.Print("Say something started.")
+
+    http.HandleFunc("/", handler)
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
